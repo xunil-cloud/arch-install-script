1a00619ae26f798bab727c74b931b28ea358cf6b
benson <freed602om@gmail.com>
2020-06-17 15:45:53 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+EFI_partition = 
+EFI_mount_point = /efi
+ROOT_partition = 
+HOME_partition = 
+UEFI = true
+
+timedatectl set-ntp true
+
+# echo PARTED
+# parted $DEVICE mklabel msdos
+# parted $DEVICE mkpart primary ext4 1MiB 500MiB
+# parted $DEVICE mkpart primary ext4 500MiB 15GiB
+# parted $DEVICE set 1 boot on
+
+
+echo MKFS
+mkfs.ext4 ${ROOT_partition}
+mkfs.ext4 ${HOME_partition}
+
+echo MOUNT
+mount ${ROOT_partition} /mnt
+mkdir /mnt/home --verbose
+mkdir /mnt${EFI_mount_point} --verbose
+mount ${EFI_partition} /mnt/efi
+mount ${HOME_partition} /mnt/home
+
+echo MIRROR
+vim /etc/pacman.d/mirrorlist
+
+echo INSTALL
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo CHROOT
+arch-chroot /mnt post_install.sh
+echo FINISH_CHROOT
+umount -R /mnt
+
---
+++ post_install.sh
@@ -0,0 +1,46 @@
+HOSTNAME = 
+BOOT_LOADER_NAME = ArchLinux
+EFI_mount_point = /efi
+
+install_networkmanager = true
+wifi = false
+
+
+ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
+hwclock --systohc
+
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+sed -i 's/#zh_TW.UTF-8/zh_TW.UTF-8/g' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo $HOSTNAME > /etc/hostname
+echo -e "127.0.0.1 localhost\n::1       localhost\n127.0.1.1 ${HOSTNAME}.localdomain ${HOSTNAME}" > /etc/hosts
+
+pacman -S vim base-devel man-db grub efibootmgr
+
+
+echo -e "set root password: \n"
+passwd
+
+pacman -S sudo xdg-user-dirs
+echo -e "create billson user: \n"
+useradd billson -m -G wheel
+echo -e "set billson password: \n"
+passwd billson
+sudo -u billson xdg-user-dirs-update
+EDITOR=vim visudo
+
+if [ ${install_networkmanager} ]; then
+	pacman -S networkmanager
+    systemctl enable networkmanager
+else
+	pacman -S dhcpcd
+    systemctl enable dhcpcd
+fi
+
+
+
+grub-install --target=x86_64-efi --efi-directory=${EFI_mount_point} --bootloader-id=${BOOT_LOADER_NAME}
+grub-mkconfig -o /boot/grub/grub.cfg
+
